ce0243f98f3b79ff8e54a9da7ec9c74010964b11
Josephine Mui <josephinemui@yahoo.com>
2026-02-24 13:45:33 -0600

    Add request package

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
