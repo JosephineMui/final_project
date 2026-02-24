8dd2f592b5d36aed7b0366ad7119c4413dbbf087
Josephine Mui <josephinemui@yahoo.com>
2026-02-24 12:36:25 -0600

    Add statement to run code

--- server.py
+++ server.py
@@ -9,3 +9,10 @@ def emotion_detector():
 
     scores = emotion_detector(text_to_analyze)
     return jsonify(f"For the given statement, the system response is {scores}")
+
+if __name__ == "__main__":
+    # This function executes the flask app and deploys it on localhost:5000
+    # app.run(host='0.0.0.0', port=5000)
+
+    # Run the Flask app with debug mode enabled for development purposes
+    app.run(debug=True)
