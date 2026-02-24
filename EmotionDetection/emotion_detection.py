27d05cb9e5809ef8c5d78f80fe30d0ae1157e56a
Josephine Mui <josephinemui@yahoo.com>
2026-02-24 14:47:43 -0600

    Added error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,11 +3,22 @@ import requests
 
 URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-#Input json: { "raw_document": { "text": text_to_analyse } }
 
 def emotion_detector(text_to_analyze):
+    # Check if the input text is empty or blank
+    # if not text_to_analyze.strip():
+    #     return {"message": "Input text is empty or blank. Please provide valid text for emotion detection."}
+
     payload = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(URL, headers=HEADERS, json=payload)
+    if response.status_code == 400:
+        return {"anger": "None",     
+                "disgust": "None",
+                "fear": "None",
+                "joy": "None",
+                "sadness": "None",
+                "dominant_emotion": "None"}
+    
     scores = {}
     if response.status_code == 200:
         formatted_response = json.loads(response.text)
